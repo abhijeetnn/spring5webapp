6ba87c1d23e17e9dfd8da19b05b0a8b7b81b438a
Abhijeet <abhijeetnn@gmail.com>
2020-12-21 19:52:48 +0530

    Test

---
+++ src/main/java/guru/springframework/spring5webapp/Foo.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5webapp;
+
+public class Foo {
+   private int number;
+	
+   public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+
+   
+}
